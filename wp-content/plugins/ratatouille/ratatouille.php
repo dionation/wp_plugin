24b6e122d255a06537f17288118afd50335c269f
dionation <dionation@gmail.com>
2019-03-19 10:57:42 +0100

    1. Création d'un plugin avec la commande wp scaffold plugin nomduplugin,on peut retrouver notre plugin dans le dossier wp-content/plugins/ on peut dans notre plugin supprimer tous les fichiers mis a part le fichier 'nomduplugin.php'

---
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     Ratatouille
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     ratatouille
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ratatouille
+ */
+
+// Your code starts here.
+
