784e6d33d2f50efb122f48f4f015de7a1c02b5b6
dionation <dionation@gmail.com>
2019-03-20 15:21:36 +0100

    2. Création d'une function personnalisé register_recipe_post_type qui contient une function prédéfinie de wordpress du nom de register_post_type, cette function attend au moins deux paramètre, le premier c'est son slug(identifiant) et le second cest 'public' avec comme valeur true ou false, ce paramètre définira si oui ou non votre plugin est visible dans le menu de wordpress admin,et pour finir on initialise notre function grâce a 'add_action' au moment du hook 'init'

--- wp-content/plugins/ratatouille/ratatouille.php
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -14,3 +14,17 @@
 
 // Your code starts here.
 
+
+// Minimum requis pour que le plugin après avoir été activé dans les plugins,apparaissent dans le menu à gauche.
+// Il apparaîtra sous le nom 'Article' car c'est un nom attribué par défaut,nous verrons dans le prochain commit comment modifier cela.
+function register_recipe_post_type(){
+  // register_post_type permet de rajouter un post-type dans votre menu, de base il existe des post type tel que 'PAGE' ou 'POST'(article) et bien d'autre,notre but va être de créer un nouveau post type qui sera 'Recipe'(recette en français)
+  // https://developer.wordpress.org/reference/functions/register_post_type/
+  register_post_type('recipe',
+  [
+    'public' => true
+    ]
+
+  );
+}
+add_action('init','register_recipe_post_type');
