a2306678e3b7355b8bf17d1e67e8db1870a13b5c
dionation <dionation@gmail.com>
2019-03-21 10:34:35 +0100

    3. Rajout d'un paramètre dans register_post_type nous permetant d'obtenir une page regroupant toutes nos recettes

--- wp-content/plugins/ratatouille/ratatouille.php
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -22,7 +22,17 @@ function register_recipe_post_type(){
   // https://developer.wordpress.org/reference/functions/register_post_type/
   register_post_type('recipe',
   [
-    'public' => true
+    // Labels est un paramètre contenant un tableau dans le quel vous passerez plein de paramètre prédéfinie que vous retrouverez dans la documentation,parmis eux 'name' qui permet de modifier le nom par defaut qui est 'Article' que l'on va passé à 'Recette' 
+    'labels' => [
+      // Nom au pluriel apparent dans le menu ou quand c'est nécéssaire dans une phrase
+      'name' => 'Recettes',
+      // Nom au singulier 
+      'singular_name' => 'Recette'
+
+    ],
+    'public' => false,
+    // has_archive vous permet de vous rendre sur http://localhost:8080/index.php/recipe/ et d'y trouver toutes vos recettes si cela ne fonctionne pas c'est possible qu'il vous faille réecrire vos permaliens pour ce faire allez dans votre backoffice -> Settings -> Permaliens et cliquez sur Enregistrer les modifications,après avoir fait ça testez en passant de true a false actualisant votre page http://localhost:8080/index.php/recipe/ observez, repassez de false à true et actualisez puis observez.
+    'has_archive' => true,
     ]
 
   );
