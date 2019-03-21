c377062a3bd11c4d96cf13571b89da86e860468d
dionation <dionation@gmail.com>
2019-03-21 11:33:03 +0100

    4. Rajout d'un paramètre permetant de réecrire le slug de nos recettes

--- wp-content/plugins/ratatouille/ratatouille.php
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -30,9 +30,17 @@ function register_recipe_post_type(){
       'singular_name' => 'Recette'
 
     ],
-    'public' => false,
+    'public' => true,
     // has_archive vous permet de vous rendre sur http://localhost:8080/index.php/recipe/ et d'y trouver toutes vos recettes si cela ne fonctionne pas c'est possible qu'il vous faille réecrire vos permaliens pour ce faire allez dans votre backoffice -> Settings -> Permaliens et cliquez sur Enregistrer les modifications,après avoir fait ça testez en passant de true a false actualisant votre page http://localhost:8080/index.php/recipe/ observez, repassez de false à true et actualisez puis observez.
     'has_archive' => true,
+
+    // Dans le commit précédent on pouvait retrouver nos recettes avec l'urls finissant par localhost.../recipe/ a présent grâce au rewrite et a son paramètre slug on peut changer le slug de notre url pour que ce soit localhost.../recette/
+    // A savoir: Dés que vous touchez a l'url même dans le code il faut allez dans me backoffice puis settings->permalien et enregistrer les modifications sinon cela ne marchera pas.
+    // Note : l'url de nos recettes ont aussi changé : http://localhost:8080/index.php/recette/nomdelarecette
+    // Alors qu'avant c'était : http://localhost:8080/index.php/recipe/nomdelarecette
+    'rewrite' => [
+      'slug' => 'recette'
+    ]
     ]
 
   );
