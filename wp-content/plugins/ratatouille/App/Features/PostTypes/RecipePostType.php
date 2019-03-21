df4b354c13c16aee03868b2ddba41b007dc5c2f6
dionation <dionation@gmail.com>
2019-03-21 23:47:51 +0100

    6. Création d'un namespace dans RecipePostType.php, et utilisation de ce namespace dans ratatouille.php ligne 23

--- wp-content/plugins/ratatouille/App/Features/PostTypes/RecipePostType.php
+++ wp-content/plugins/ratatouille/App/Features/PostTypes/RecipePostType.php
@@ -1,4 +1,6 @@
 <?php
+// On définit un nom qu'on imagine unique vu sa complexité, une bonne pratique est que le nom soit équivalent au chemin pour atteindre le fichier dans le quel on se trouve.
+namespace App\Features\PostTypes;
 
 // On à Couper Coller notre function du fichier Ratatouille.php pour l'englobé avec une class
 class RecipePostType
--- wp-content/plugins/ratatouille/ratatouille.php
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:     Ratatouille
  * Plugin URI:      PLUGIN SITE HERE
@@ -18,6 +19,6 @@
 // http://php.net/manual/fr/function.require-once.php
 require_once('App/Features/PostTypes/RecipePostType.php');
 
-// Appel de la class pour pouvoir utiliser sa function register
-add_action('init',[RecipePostType::class, 'register']);
+// Appel de la class venant du namespace qu'on a définit dans le fichier RecipePostType.php ,l'avantage c'est qu'on a redéfinit le nom de notre class qui était RecipePostType par App\Features\PostTypes\RecipePostType il y a maintenant presque aucune chance qu'un autre plugin utilise le meme nom de class et donc il n'y aura pas de conflit.
+add_action('init',[App\Features\PostTypes\RecipePostType::class, 'register']);
 
