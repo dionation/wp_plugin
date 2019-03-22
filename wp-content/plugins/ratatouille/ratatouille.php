7d99f2643d231aa8710c492eba0a49ccbdd58130
dionation <dionation@gmail.com>
2019-03-22 09:27:36 +0100

    8. Création d'un fichier autoload qui généra un require_once a chaque fois qu'une class est réclamée par un processus

--- wp-content/plugins/ratatouille/ratatouille.php
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -1,5 +1,5 @@
 <?php
-// On use ici le namespace, l'avantage de cette écriture là c'est qu'on créer un 'ALIAS' avec la commande 'as' ce qui fait qu'a la ligne 24 on peut simplement écrire RecipePostType::class, pour l'exemple j'ai préciser que l'alias était RecipePostType mais a vrai dir c'est nécéssaire qeu si on modifie le nom de la class si on veut garder un Alias qui porte le meme nom que la class il ne faut pas le préciser si on écrivait :
+// On use ici un namespace (celui qu'on créer ligne 2 dans autoload.php), l'avantage de cette écriture là c'est qu'on créer un 'ALIAS' avec la commande 'as' ce qui fait qu'a la ligne 24 on peut simplement écrire RecipePostType::class, pour l'exemple j'ai précisé que l'alias était RecipePostType mais a vrai dir c'est nécéssaire que si on modifie le nom de la class si on veut garder un Alias qui porte le meme nom que la class il ne faut pas le préciser si on écrivait :
 // use App\Features\PostTypes\RecipePostType cela reviendrait au même un créer automatiqement un alias du même nom si on ne le précise pas.
 use App\Features\PostTypes\RecipePostType as RecipePostType;
 
@@ -17,9 +17,9 @@ use App\Features\PostTypes\RecipePostType as RecipePostType;
  */
 
 // Your code starts here.
+// on require_once le fichier autload.php qui lui fera un require_once pour toutes les class qui ont besoin d'être chargées.
+require_once('autoload.php');
 
-require_once('App/Features/PostTypes/RecipePostType.php');
-
-
+// require_once('App/Features/PostTypes/RecipePostType.php');
 add_action('init',[RecipePostType::class, 'register']);
 
