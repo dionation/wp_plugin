679a8761e7949972f9737343986f85663bf7b2b3
dionation <dionation@gmail.com>
2019-03-24 01:03:41 +0100

    14. Initialisation d'une taxnomy avec des valeurs par défaut

---
+++ wp-content/plugins/ratatouille/App/Features/Taxonomies/RecipeTaxonomy.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Features\Taxonomies;
+use App\Features\PostTypes\RecipePostType;
+
+
+
+// Voici le strict nécéssaire, j'ai simplement créer une class, avec une function que j'ai appelé register et cette function lance la function prédéfinie register_taxonomy avec comme premier paramètre doit être un identifiant,le second argument est à qui on va attribué notre taxonomy, on pourrait y mettre 'post' ou 'page' cest a dire qu'on rendrais accessible notre taxonomy dans les article ou dans les pages, à vous d'essayer. Mais pour le coup ici on rend notre taxonomy accessible dans notre post_type recette en ciblant son identifiant. Le troisème argument est optionnel nous verrons cela au prochain commit.
+// Vous pouvez observez qu'une taxonomy à bien été rajouté mais qu'elle s'appelle "Etiquette",c'est un nom qui est donné par défaut car au final tout ce qu'on a fait c'est uniquement initialiser une taxonomy avec l'aide de la function register_taxonomy et de la raccroché à notre post_type recette.
+
+class RecipeTaxonomy {
+
+  public static $slug = 'recipe_taxonomy';
+
+  /**
+   * Enregistrement de la Taxonomie
+   * https://developer.wordpress.org/reference/functions/register_taxonomy/
+   * @return void
+   */
+  public static function register(){
+    register_taxonomy(self::$slug, [RecipePostType::$slug],'');
+  }
+}
--- wp-content/plugins/ratatouille/ratatouille.php
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -2,7 +2,7 @@
 // On use ici un namespace (celui qu'on créer ligne 2 dans autoload.php), l'avantage de cette écriture là c'est qu'on créer un 'ALIAS' avec la commande 'as' ce qui fait qu'a la ligne 24 on peut simplement écrire RecipePostType::class, pour l'exemple j'ai précisé que l'alias était RecipePostType mais a vrai dir c'est nécéssaire que si on modifie le nom de la class si on veut garder un Alias qui porte le meme nom que la class il ne faut pas le préciser si on écrivait :
 // use App\Features\PostTypes\RecipePostType cela reviendrait au même un créer automatiqement un alias du même nom si on ne le précise pas.
 use App\Features\PostTypes\RecipePostType as RecipePostType;
-
+use App\Features\Taxonomies\RecipeTaxonomy;
 /**
  * Plugin Name:     Ratatouille
  * Plugin URI:      PLUGIN SITE HERE
@@ -20,6 +20,6 @@ use App\Features\PostTypes\RecipePostType as RecipePostType;
 // on require_once le fichier autload.php qui lui fera un require_once pour toutes les class qui ont besoin d'être chargées.
 require_once('autoload.php');
 
-// require_once('App/Features/PostTypes/RecipePostType.php');
 add_action('init',[RecipePostType::class, 'register']);
 
+add_action('init', [RecipeTaxonomy::class, 'register']);
