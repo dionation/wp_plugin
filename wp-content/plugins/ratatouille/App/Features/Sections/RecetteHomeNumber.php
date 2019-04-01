012e5e85ef789ed90d31eeaa34ef895ee3cc5bc6
dionation <dionation@gmail.com>
2019-04-01 16:27:10 +0200

    45. Création d'un dossier Sections et d'une class RecetteHomeNumber pour initialiser une section qu'on retrouvera dans l'administration->settings->reading en bas de la page

---
+++ wp-content/plugins/ratatouille/App/Features/Sections/RecetteHomeNumber.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Features\Sections;
+class RecetteHomeNumber
+{
+  /**
+   * Enregistrement de la section
+   *
+   * @return void
+   */
+  public static function init()
+  {
+    // https://developer.wordpress.org/reference/functions/add_settings_section/
+    add_settings_section(
+      'recette-home-number', // l'id (slug) de la section
+      __('Nombre de recette sur la home'), // le titre de la section qui apparaîtra
+      [self::class, 'render'], // la méthode qui affichera le contenu de la secttion
+      'reading' // l'id de la page à la quelle on ajoute la section
+    );
+  }
+  /**
+   * fonction pour render le contenu de la section
+   */
+  public static function render()
+  {
+    echo 'Ceci est ma première section';
+  }
+}
