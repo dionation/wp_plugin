5496e07959164028d1c24f1418b54e2f6aa7d36b
dionation <dionation@gmail.com>
2019-04-01 16:35:57 +0200

    46. Création d'un fichier Section.php ou l'on va retrouver toutes nos futurs sections,pour le moment on en qu'une seule. Du coups on change l'appel dans hooks.php on fait appel a Section::class et dans le fichier Section.php là on fait appel a la class RecetteHomeNumber

--- wp-content/plugins/ratatouille/hooks.php
+++ wp-content/plugins/ratatouille/hooks.php
@@ -3,7 +3,7 @@ use App\Features\PostTypes\RecipePostType as RecipePostType;
 use App\Features\Taxonomies\RecipeTaxonomy;
 use App\Features\MetaBoxes\RecipeDetailsMetabox;
 use App\Features\Widgets\DishOfTheDayWidget;
-use App\Features\Sections\RecetteHomeNumber;
+use App\Features\Sections\Section;
 
 add_action('init',[RecipePostType::class, 'register']);
 add_action('init', [RecipeTaxonomy::class, 'register']);
@@ -11,4 +11,4 @@ add_action('add_meta_boxes_recipe', [RecipeDetailsMetabox::class, 'add_meta_box'
 add_action('save_post_' . RecipePostType::$slug, [RecipeDetailsMetabox::class, 'save']);
 add_action('widgets_init', [DishOfTheDayWidget::class, 'register']);
 
-add_action('admin_init',[RecetteHomeNumber::class,'init']);
+add_action('admin_init',[Section::class,'init']);
