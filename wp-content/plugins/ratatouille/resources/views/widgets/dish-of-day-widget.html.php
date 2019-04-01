e3264868bf124d9c778dca1d4887678ad8f68f4a
dionation <dionation@gmail.com>
2019-04-01 12:12:42 +0200

    42. Envoi des données dans notre vue dish-of-day-widget et affichage conditionnel

--- wp-content/plugins/ratatouille/resources/views/widgets/dish-of-day-widget.html.php
+++ wp-content/plugins/ratatouille/resources/views/widgets/dish-of-day-widget.html.php
@@ -1,11 +1,19 @@
 <div class="widget-wrapper">
-<!-- Pour le moment on affiche du texte en dur,ce n'est pas le texte qui sera enregistré par le formulaire dans le backoffice -->
-  <h4> Le titre </h4>
+<!-- On utilise les variables passées par le compact de notre method widget() ligne 32 dans DishOfTheDayWidget.php, on utilise ça sous condition qu'elle existe pour ne pas avoir d'erreur,si on essaie d'affiché le contenu d'une variable alors qu'elle est vide il va nous donné une erreur -->
+  <?php if (!empty($instance['title'])) : ?>
+  <h4> <?php echo $instance['title']; ?></h4>
+  <?php endif; ?>
 
 
   <div class="textwidget">
 
-  Le texte 
+  <?php 
+  // Pareil on affiche si la variable n'est pas vide et en même temps on assaini ce qu'on récupère avec la method esc_html__
+  // https://developer.wordpress.org/reference/functions/esc_html__/
+  if (!empty($instance['text'])) : 
+  echo esc_html__($instance['text']); 
+  endif; 
+  ?>
 
   </div>
 
