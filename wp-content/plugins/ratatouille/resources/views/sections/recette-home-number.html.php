e30f20f094193c924ea2c235da84136113a09855
dionation <dionation@gmail.com>
2019-04-02 11:53:35 +0200

    48. On utilise la function prédéfinie register_setting() qui nous permet d'enregistrer notre contenu dans la BDD et on donne un name à notre input pour que register_setting() récupère sa valeur

--- wp-content/plugins/ratatouille/resources/views/sections/recette-home-number.html.php
+++ wp-content/plugins/ratatouille/resources/views/sections/recette-home-number.html.php
@@ -1,1 +1,2 @@
-<input type="number" name="" id=""> 
+<!-- Je lui donne un name pour que notre function register_setting puisse récupérer la valeur qu'on a entré dans notre input. -->
+<input type="number" name="recette_home_number" id=""> 
