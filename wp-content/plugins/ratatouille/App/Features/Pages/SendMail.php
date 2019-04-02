5f1f3cab4f2b364fd5278b3aa716fd2f90a42355
dionation <dionation@gmail.com>
2019-04-02 16:58:18 +0200

    50. Ajout d'un lien dans le menu qui renverra vers une page personnalisée afin de contacter nos clients avec un formulaire de contact.

---
+++ wp-content/plugins/ratatouille/App/Features/Pages/SendMail.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Features\Pages;
+class SendMail
+{
+  /**
+   * Initialisation de la page.
+   *
+   * @return void
+   */
+  public static function init()
+  {
+    //https: //developer.wordpress.org/reference/functions/add_menu_page/  
+    add_menu_page(
+      __('Formulaire pour contacter les clients'), // Le titre qui s'affichera sur la page
+      __('Mail Client'), // le texte dans le menu
+      'edit_private_pages', // la capacité qu'il faut posséder en tant qu'utilisateur pour avoir accès à cette page (les roles et capacité seront vue plus tard)
+      'mail-client', // Le slug du menu
+      [self::class, 'render'], // La méthode qui va afficher la page
+      'dashicons-email-alt', // L'icon dans le menu
+      26 // la position dans le menu (à comparer avec la valeur deposition des autres liens menu que l'on retrouve dans la doc).
+    );
+  }
+  /**
+   * Affichage de la page
+   *
+   * @return void
+   */
+  public static function render()
+  {
+    echo "hello dude";
+  }
+}
--- wp-content/plugins/ratatouille/hooks.php
+++ wp-content/plugins/ratatouille/hooks.php
@@ -4,11 +4,13 @@ use App\Features\Taxonomies\RecipeTaxonomy;
 use App\Features\MetaBoxes\RecipeDetailsMetabox;
 use App\Features\Widgets\DishOfTheDayWidget;
 use App\Features\Sections\Section;
+use App\Features\Pages\Page;
 
 add_action('init',[RecipePostType::class, 'register']);
 add_action('init', [RecipeTaxonomy::class, 'register']);
 add_action('add_meta_boxes_recipe', [RecipeDetailsMetabox::class, 'add_meta_box']); 
 add_action('save_post_' . RecipePostType::$slug, [RecipeDetailsMetabox::class, 'save']);
 add_action('widgets_init', [DishOfTheDayWidget::class, 'register']);
-
 add_action('admin_init',[Section::class,'init']);
+
+add_action('admin_menu',[Page::class,'init']);
