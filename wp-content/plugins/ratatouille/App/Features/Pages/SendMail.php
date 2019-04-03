be9ae6b383b8e9ff545498c22425aab3422411f9
dionation <dionation@gmail.com>
2019-04-03 09:16:37 +0200

    53. On ajoute une sécurité, pour assainir les données envoyées par l'utilisateur

--- wp-content/plugins/ratatouille/App/Features/Pages/SendMail.php
+++ wp-content/plugins/ratatouille/App/Features/Pages/SendMail.php
@@ -33,10 +33,10 @@ class SendMail
   public static function send_mail()
   {
     // Nous récupérons les données envoyé par le formulaire qui se retrouve dans la variable $_POST
-    $email = $_POST['email'];
-    $name = $_POST['name'];
-    $firstname = $_POST['firstname'];
-    $message = $_POST['message'];
+    $email = sanitize_email($_POST['email']);
+    $name = sanitize_text_field($_POST['name']);
+    $firstname = sanitize_text_field($_POST['firstname']);
+    $message = sanitize_textarea_field($_POST['message']);
     // la fonction wordpress pour envoyer des mails https://developer.wordpress.org/reference/functions/wp_mail/
     wp_mail($email, 'Pour ' . $name . ' ' . $firstname, $message);
     // la fonction wp_safe_redirect redirige vers une url. La fonction wp_get_referer renvoi vers la page d'ou la requête a été envoyé.
