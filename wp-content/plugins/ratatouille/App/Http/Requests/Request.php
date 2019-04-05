c91dd8956bd2183257355d5b16eea919cea2d625
dionation <dionation@gmail.com>
2019-04-05 09:27:16 +0200

    63. On continue avec le fichier Request.php ,on rajout un foreach dans notre methode 'validation' pour le moment cela n'est pas complet on prend le temps de décortiquer en plusieurs commits jusqu'a ce que cela soit fonctionnel

--- wp-content/plugins/ratatouille/App/Http/Requests/Request.php
+++ wp-content/plugins/ratatouille/App/Http/Requests/Request.php
@@ -9,8 +9,21 @@ class Request{
 
   // On créer une function du nom de validation qui attend un paramètre de type array.Ce paramètre va être rempli via le fichier SendMail.php ligne 44.  
   public static function validation(array $data){
+  // Pour chaque entrée on lance une des méthode ci dessous selon la valeur du tableau $data
+  // Data est un tableau de clé => valeur dont la clé est le "name" de l'input et la valeur est la méthode de vérification que l'on veut appliquer sur le contenu de l'input. C'est nous qui avons fait en sorte que les valeurs du tableau $data correspondent aux names de nos inputs dans notre formulaire.
+  foreach ($data as $input_name => $verification) {
+    // on lance la function de la class, 'email' ou 'required' selon ce que vaut $verification et on rempli le paramètre de la function avec $input_name
+    call_user_func([self::class, $verification], $input_name);
+  }
   
   }
 
+  public static function required(string $input){
+
+  }
+
+  public static function email(string $input){
+    
+  }
 
 }
