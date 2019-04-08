c32c490a39b90e8be6f03d77559adf46e82c5518
dionation <dionation@gmail.com>
2019-04-08 19:23:22 +0200

    71. Ajout d'une fonctionnalité pour sauvegarder les mails dans la base de donnée une fois que le mail à été envoyé. Dans ce commit je n'ai pas mis le lien de toutes les nouvelles fonction. Il faut prendre le reflexe d'aller voir dans la documentation lorsque vous voyez une nouvelle fonction. Ou si vous avez le plugin intelephense de survoler la fonction afin de l'analyser

--- wp-content/plugins/ratatouille/App/Http/Controllers/MailController.php
+++ wp-content/plugins/ratatouille/App/Http/Controllers/MailController.php
@@ -39,6 +39,22 @@ class MailController
         'status' => 'success',
         'message' => 'votre e-mail a bien été envoyé'
       ];
+
+      // Nous allons également sauvegarder en base de donnée les mails que nous avons envoyé.
+      global $wpdb;
+      // Nous utilisons une fonction pour insérer dans la db. https://developer.wordpress.org/reference/classes/wpdb/insert/
+      $wpdb->insert(
+        $wpdb->prefix . 'rat_mail', // premier argument est le nom de la table. c'est la deuxième fois que l'on écrit ce nom. Il serait bon de faire un refactoring et d'utiliser une constante à la place. Nous le ferons plus tard.
+        [ // Deuxième paramêtre est un tableau dont la clé est le nom de la colonne dans la table et la valeur est la valeur à mettre dans la colonne
+        // Colonne => Valeur
+          'userid' => get_current_user_id(),
+          'lastname' => $name,
+          'firstname' => $firstname,
+          'email' => $email,
+          'content' => $message,
+          'created_at' => current_time('mysql')
+        ]
+      );
     } else {
       $_SESSION['notice'] = [
         'status' => 'error',
