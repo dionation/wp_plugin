c7eed4a8a3a511883f2be4b3b6d865a5ee9277a9
dionation <dionation@gmail.com>
2019-04-08 19:16:59 +0200

    70. Ajout d'un hook qui ne se déclanche qu'à l'activation du plugin. Nous profitons de ce hook afin de créer notre propre table dans la base de donnée. Nous organisons tout de suite les dossiers et fichier afin de commencé dès maintenant à connaître les bonnes pratiques et de nous préparer à la structure de framwork comme laravel.

--- wp-content/plugins/ratatouille/hooks.php
+++ wp-content/plugins/ratatouille/hooks.php
@@ -7,7 +7,7 @@ use App\Features\Sections\Section;
 use App\Features\Pages\Page;
 use App\Http\Controllers\MailController;
 use App\Setup;
-
+use App\Database\Database;
 add_action('init',[RecipePostType::class, 'register']);
 add_action('init', [RecipeTaxonomy::class, 'register']);
 add_action('add_meta_boxes_recipe', [RecipeDetailsMetabox::class, 'add_meta_box']); 
@@ -16,5 +16,7 @@ add_action('widgets_init', [DishOfTheDayWidget::class, 'register']);
 add_action('admin_init',[Section::class,'init']);
 add_action('admin_menu',[Page::class,'init']);
 add_action('admin_action_send-mail', [MailController::class, 'send']);
-
 add_action('admin_init', [Setup::class, 'start_session']);
+// On ajoute la méthode qui va s'executer lors de l'activation du plugin
+// Cette fonction ne s'active que lors de l'activation du plugin https://developer.wordpress.org/reference/functions/register_activation_hook/
+register_activation_hook(__DIR__ . '/ratatouille.php', [Database::class, 'init']);
