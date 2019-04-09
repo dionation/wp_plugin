a76e58b3983210e7ce881379efc92e3599e0fced
dionation <dionation@gmail.com>
2019-04-09 21:44:29 +0200

    86. Rajout d'un formulaire de suppression dans les pages des mails

--- wp-content/plugins/ratatouille/resources/views/pages/show-mail.html.php
+++ wp-content/plugins/ratatouille/resources/views/pages/show-mail.html.php
@@ -23,6 +23,12 @@
                 </div>
             </div>
             <a href="<?php menu_page_url('mail-client'); ?>" class="button button-primary">retour</a>
+            <!-- On rajout ici un formulaire qui ne contient qu'un bouton ainsi qu'un input mais caché (hidden) on le cache car ce n'est pas nécessaire de le voir par contre on va avoir besoin de ce qu'il contient -->
+            <form action="<?php get_site_url(); ?>?action=mail-delete" method="post">
+            <!-- On met un input hidden avec comme valeur l'id du mail en question on fait ça pour en suit récupérer l'id via $_POST['et le NAME qui est ici "id" '] on récupérera ca valeur dans les prochain commit pour l'instant on à uniquement un petit formulaire qui contient l'id du mail et qui nous met une action=delete dans notre url -->
+                <input type="hidden" name="id" value="<?= $mail->id; ?>">
+                <button type="submit" class="button">supprimer</button>
+            </form>
         </div>
     </div>
 </div>
