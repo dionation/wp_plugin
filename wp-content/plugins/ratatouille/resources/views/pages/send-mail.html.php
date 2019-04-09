47c4178b43e612cc6732795712019b67c16d3b7f
dionation <dionation@gmail.com>
2019-04-09 12:59:34 +0200

    73. Modification de send-mail.html.php ,rajout d'une row avec 2 col-sm-6 pour l'instant le visuel ne donne rien car les classrow et col-sm-6 sont des class bootstrap et qu'on utilise pas bootstrap, nous verrons au prochain commit comment utiliser un fichier de style

--- wp-content/plugins/ratatouille/resources/views/pages/send-mail.html.php
+++ wp-content/plugins/ratatouille/resources/views/pages/send-mail.html.php
@@ -3,35 +3,50 @@
     <h1><?= get_admin_page_title(); ?></h1>
     <!-- Ici nous ajoutons une partie d'html qui prendra en charge les notifications. On met cela dans un fichier à part afin de pouvoir réutiliser les notifications ailleurs -->
     <?php view('partials/notice'); ?>
-    <p>Ce formulaire vous permet de contacter vos clients pour leur réservation.</p>
-    <form action="<?= get_admin_url() . '?action=send-mail'; ?>" method="post">
-    <!-- Cette fonction créer des inputs cachés qui contiennent des informations qui vont nous permetre de savoir si le formulaire est authentique et si il est bien executé via notre site web et pas via une autre source. -->
-    <?php wp_nonce_field('send-mail'); ?>
+    <div class="row">
+        <div class="col-sm-6">
 
-        <table class="form-table">
-            <tr>
-                <th>e-mail</th>
-                <!-- Lorsqu'on affiche le formulaire sans être passé par les validations aucune clé old na été enregistré dans la sessions, ceci créer une erreur si l'on demande de l'affichée,c'est pour cela que l'on met une condition,on demande de l'affichée que si elle existe -->
-                <td><input type="email" name="email" id="email" value="<?= isset($old['email']) ? $old['email'] : '' ?>"></td>
-            </tr>
-            <tr>
-                <th>Nom</th>
-                <td><input type="text" name="name" id="name" value="<?= isset($old['name']) ? $old['name'] : '' ?>"></td>
-            </tr>
-            <tr>
-                <th>Prénom</th>
-                <td><input type="text" name="firstname" id="firstname" value="<?= isset($old['firstname']) ? $old['firstname'] : '' ?>"></td>
-            </tr>
-            <tr>
-                <th>Message</th>
-                <td><textarea name="message" id="message" cols="30" rows="10"><?= isset($old['message']) ? $old['message'] : '' ?></textarea></td>
-            </tr>
-            <tr>
-                <th></th>
-                <td><button type="submit" class="button-primary">Envoyer</button></td>
-            </tr>
+            <p>Ce formulaire vous permet de contacter vos clients pour leur réservation.</p>
+            <form action="<?= get_admin_url() . '?action=send-mail'; ?>" method="post">
+                <!-- Cette fonction créer des inputs cachés qui contiennent des informations qui vont nous permetre de savoir si le formulaire est authentique et si il est bien executé via notre site web et pas via une autre source. -->
+                <?php wp_nonce_field('send-mail'); ?>
 
-        </table>
-    </form>
-</div>  
+                <table class="form-table">
+                    <tr>
+                        <th>e-mail</th>
+                        <!-- Lorsqu'on affiche le formulaire sans être passé par les validations aucune clé old na été enregistré dans la sessions, ceci créer une erreur si l'on demande de l'affichée,c'est pour cela que l'on met une condition,on demande de l'affichée que si elle existe -->
+                        <td><input type="email" name="email" id="email"
+                                value="<?= isset($old['email']) ? $old['email'] : '' ?>"></td>
+                    </tr>
+                    <tr>
+                        <th>Nom</th>
+                        <td><input type="text" name="name" id="name"
+                                value="<?= isset($old['name']) ? $old['name'] : '' ?>"></td>
+                    </tr>
+                    <tr>
+                        <th>Prénom</th>
+                        <td><input type="text" name="firstname" id="firstname"
+                                value="<?= isset($old['firstname']) ? $old['firstname'] : '' ?>"></td>
+                    </tr>
+                    <tr>
+                        <th>Message</th>
+                        <td><textarea name="message" id="message" cols="30"
+                                rows="10"><?= isset($old['message']) ? $old['message'] : '' ?></textarea></td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td><button type="submit" class="button-primary">Envoyer</button></td>
+                    </tr>
 
+                </table>
+            </form>
+        </div>
+        <div class="col-sm-6">
+            <div class="postbox">
+                <div class="inside">
+                    <strong>client : </strong>
+                </div>
+            </div>            
+        </div>
+    </div>
+</div>
