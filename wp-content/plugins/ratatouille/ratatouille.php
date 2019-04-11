09ca11d7749b73d1ee45128d79e5166ab7b35512
dionation <dionation@gmail.com>
2019-04-11 16:21:03 +0200

    107. Fin de notre plugin ratatouille! un petit mot vous attend dans le fichier principal ratatouille.php

--- wp-content/plugins/ratatouille/ratatouille.php
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -17,3 +17,7 @@
 require_once('autoload.php');
 
 require_once('bootstrap.php');
+
+// Message de Yassine Kharchaf, le coach des coachs
+// Ce tutoriel est maintenant fini. Vous avez appris à vous familiarisez avec wordpress, vue le language php et appris à programmer en orienté objet de manière structure. La structure que l'on a mise en place n'est pas la seul possibilité ni la seul façon de faire. A vous de trouver votre structure en vous inspirant de framework comme laravel ou encore symfoni ou autre. Nous n'avons vue ensemble qu'une petite partie de wordpress et des possibilités. A vous de vous challenger maintenant en essayent d'aller plus loin et en étant créatif pour imaginer des challenges et leur solution. Il y aura encore des commits après celui-ci qui auront pour but de faire un gros refactoring. Car oui plus on pense son code mieux on le pense. Mon objectif ici est de créer un design pattern (une façon de structurer son code), un mini framework (cadre de travail ) pour dévellopper proprement avec wordpress. 
+// Think, Debug, Create, Repeat !!!
