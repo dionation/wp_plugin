6be690b9c634c2c1866bf2fa933a5ee6b7af00f7
dionation <dionation@gmail.com>
2019-04-11 14:55:44 +0200

    97. On supprime ce qu'on avait fait dans ratatouille.php pour nous entrainer à manipuler des roles,on créer un nouveau dossier Roles et une class Role.php avec une function init vide, on rajoute un hook pour lancer la function init(qui est vide) a l'activiation du plugin ratatouille.php

---
+++ wp-content/plugins/ratatouille/App/Features/Roles/Role.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Features\Roles;
+class Role
+{
+  /**
+   * Fonction d'initialisation des roles
+   *
+   * @return void
+   */
+  public static function init()
+  {
+
+  }
+}
--- wp-content/plugins/ratatouille/hooks.php
+++ wp-content/plugins/ratatouille/hooks.php
@@ -8,6 +8,7 @@ use App\Features\Pages\Page;
 use App\Http\Controllers\MailController;
 use App\Setup;
 use App\Database\Database;
+use App\Features\Roles\Role;
 add_action('init',[RecipePostType::class, 'register']);
 add_action('init', [RecipeTaxonomy::class, 'register']);
 add_action('add_meta_boxes_recipe', [RecipeDetailsMetabox::class, 'add_meta_box']); 
@@ -18,6 +19,8 @@ add_action('admin_menu',[Page::class,'init']);
 add_action('admin_action_send-mail', [MailController::class, 'send']);
 add_action('admin_init', [Setup::class, 'start_session']);
 register_activation_hook(__DIR__ . '/ratatouille.php', [Database::class, 'init']);
+// Nous ajoutons des roles à l'activation du plugin.
+register_activation_hook(__DIR__ . '/ratatouille.php', [Role::class, 'init']);
 add_action('admin_enqueue_scripts', [Setup::class, 'enqueue_scripts']);
 // Hook personnalisé, c'est la combinaison du hook 'admin_action_' de wordpress avec mail-delete qui est l'action qu'on envoi dans l'url ligne 27 du fichier show-mail-html.php 
 add_action('admin_action_mail-delete', [MailController::class, 'delete']); 
